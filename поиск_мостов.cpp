3836b33e5b05d8570643df9faf53681246159260
Oper_doc <s.lit@outlook.com>
2018-08-28 18:37:49 +0300

    Update поиск_мостов.cpp

--- поиск_мостов.cpp
+++ поиск_мостов.cpp
@@ -1,18 +1,15 @@
 int V;
 vector< vector<int> > Lg;
 vector< pair<int, int> > B;
-vector<bool> u;
 vector<int> f, h;
 void dfs(int v, int p = -1, int cur = 0)/*O(E)*/{
     f[v] = h[v] = cur++;
-    u[v] = true;
     for (auto i : Lg[v]){
-        if (i == p)continue;
-        if (!u[i]){
+        if (h[i] != -1){
             dfs(i, v, cur);
             f[v] = min(f[v], f[i]);
             if (f[i] > h[v]) B.push_back({i,v});
         }
-        else f[v] = min(f[v], h[i]);
+        else if(p != i) f[v] = min(f[v], h[i]);
     }
 }
