c0f2eaa2893a6a2c0a914d5abe954f3e4a189128
Oper_doc <s.lit@outlook.com>
2019-01-12 15:27:45 +0300

    Update S_разновесная_декомпозиция.cpp

--- S_разновесная_декомпозиция.cpp
+++ S_разновесная_декомпозиция.cpp
@@ -1,88 +1,161 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
 #define p_l 2 * v + 1
 #define p_r 2 * v + 2
-const int SIZE = 100228, inf = 1e9 + 7;
-int n, l;
-int vd[SIZE], f[SIZE], d[SIZE] lcat[8 * SIZE], st[SIZE], up[SIZE], len[SIZE], w[SIZE];
-vector<int> Lg[SIZE], t[SIZE], vert[SIZE] euler;
+#define M ((l + r) / 2)
+typedef long long ll;
+using namespace std;//start
+const ll SIZE = 300228, inf = 1e12;//consts
+//tree
+int n;
+int vdata[SIZE];
+vector<int> gr[SIZE];
+//lca
+int first[SIZE];
+int d[SIZE];
+vector<int> euler;
+int lcat[8 * SIZE];
+//hld
+int down[SIZE];
+int up[SIZE];
+int len[SIZE];
+int w[SIZE];
+vector<int> vert[SIZE];
+vector<int> c[SIZE];
+vector<int> t[SIZE];
+vector<bool> is_ch[SIZE];//is_change
+//tree
 int dfs(int v, int p) {
     d[v] = (p == -1 ? 0 : d[p] + 1);
-    f[v] = euler.size();
+    first[v] = euler.size();
     euler.push_back(v);
     w[v] = 1;
-    for (auto i:Lg[v]) 
-        if (i != p) { w[v] += dfs(i, v);  euler.push_back(v); }
-    st[v] = v; up[v] = p; len[v] = 1;
+    for (auto i : gr[v]) {
+        if (i != p) {
+            w[v] += dfs(i, v);
+            euler.push_back(v);
+        }
+    }
+    down[v] = v;
+    up[v] = p;
+    len[v] = 1;
     vert[v].push_back(v);
-    for (auto i : Lg[v]) 
+    for (auto i : gr[v]) {
         if (i != p && w[i] * 2 >= w[v]) {
-            st[v] = st[i];
-            up[st[v]] = p;
-            len[st[v]]++;
-            vert[st[v]].push_back(v);
+            down[v] = down[i];
+            up[down[v]] = p;
+            len[down[v]]++;
+            vert[down[v]].push_back(v);
             break;
         }
+    }
     return w[v];
 }
-int Min(int u, int v) {return d[u] < d[v] ? u : v;}
+//lca
+inline int Min(int u, int v) {return d[u] < d[v] ? u : v;}
 int buildlca(int v, int l, int r) {
-    if (l == r - 1) {lcat[v] = euler[l]; return lcat[v];}
-    int m = (l + r) / 2;
-    lcat[v] = Min(buildlca(p_l, l, m), buildlca(p_r, m, r));
-    return lcat[v];
+    if (l == r - 1) return lcat[v] = euler[l];
+    return lcat[v] = Min(buildlca(p_l, l, M), buildlca(p_r, M, r));
 }
-int minlca(int v, int l, int r, int askl, int askr) {
-    if (askl >= r || l >= askr) return n;
-    if (l >= askl && r <= askr) return lcat[v];
-    int m = (l + r) / 2;
-    return Min(minlca(p_l, l, m, askl, askr), minlca(p_r, m, r, askl, askr));
+int minlca(int v, int l, int r, int a_l, int a_r) {
+    if (a_r <= l || r <= a_l) return n;
+    if (a_l <= l && r <= a_r) return lcat[v];
+    return Min(minlca(p_l, l, M, a_l, a_r), minlca(p_r, M, r, a_l, a_r));
 }
-int lca(int u, int v) {
+inline int lca(int u, int v) {
     u = first[u]; v = first[v];
     if (u > v) swap(u, v);
-    return minlca(0, 0, l, u, v + 1);
+    return minlca(0, 0, euler.size(), u, v + 1);
 }
-int buildhld(int v, int l, int r, int i) {
-    if (l == r - 1) {t[i][v] = vd[vert[i][l]]; return t[i][v];}
-    int m = (l + r) / 2;
-    t[i][v] = max(buildhld(p_l, l, m, i), buildhld(p_r, m, r, i));
-    return t[i][v];
-}
-int maxhld(int v, int l, int r, int askl, int askr, int i) {
-    if (l >= askr || r <= askl) return -inf;
-    if (l >= askl && r <= askr) return t[i][v];
-    int m = (l + r) / 2;
-    return max(maxhld(p_l, l, m, askl, askr, i), maxhld(p_r, m, r, askl, askr, i));
-}
-void addhld(int v, int l, int r, int pos, int val, int i) {
-    if (l == r - 1) {t[i][v] += val; return;    }
-    int m = (l + r) / 2;
-    if (pos < m) addhld(p_l, l, m, pos, val, i);
-    else addhld(p_r, m, r, pos, val, i);
-    t[i][v] = max(t[i][2 * v + 1], t[i][2 * v + 2]);
-}
-void addvertex(int v, int val) {
-    addhld(0, 0, len[st[v]], d[st[v]] - d[v], val, st[v]);
-}
-int maxway(int u, int v) {
-    int ans = -inf;
-    while (st[u] != st[v]) {
-        ans = max(ans, maxhld(0, 0, len[st[u]], d[st[u]] - d[u], len[st[u]], st[u]));
-        u = up[st[u]];
+//hld
+void push(int v, int i) {
+    if (is_ch[i][v]) {
+        is_ch[i][v] = false;
+        is_ch[i][p_l] = is_ch[i][p_r] = true;
+        c[i][p_l] = c[i][p_r] = c[i][v];
     }
-    ans = max(ans, maxhld(0, 0, len[st[u]], d[st[u]] - d[u], d[st[v]] - d[v] + 1, st[u]));
+}
+int buildhld(int v, int l, int r, int i){
+    if(r - l == 1) return t[i][v] = vert[i][l];
+    return t[i][v] = buildhld(p_l, l, M, i) + buildhld(p_r, M, r, i);
+}
+int chhld(int v, int l, int r, int a_l, int a_r, int i, int data) {
+    if (a_r <= l || r <= a_l) return is_ch[i][v] ? (r - l) * c[i][v] : t[i][v];
+    if (a_l <= l && r <= a_r) {
+        is_ch[i][v] = true;
+        return (r - l) * (c[i][v] = data);
+    }
+    push(v, i);
+    return t[i][v] = chhld(p_l, l, M, a_l, a_r, i, data) + chhld(p_r, M, r, a_l, a_r, i, data);
+}
+int sumhld(int v, int l, int r, int a_l, int a_r, int i){
+    if (a_r <= l || r <= a_l) return 0;
+    if (a_l <= l && r <= a_r) return is_ch[i][v] ? (r - l) * c[i][v] : t[i][v];
+    push(v, i);
+    int ans = sumhld(p_l, l, M, a_l, a_r, i) + sumhld(p_r, M, r, a_l, a_r, i);
+    t[i][v] = (is_ch[i][p_l] ? (M - l) * c[i][p_l]:t[i][p_l]) + (is_ch[i][p_l] ? (r - M) * c[i][p_r]:t[i][p_r]);
     return ans;
 }
-int max_q(int u, int v)
-{int uvlca = lca(u, v); return max(maxway(u, uvlca), maxway(v, uvlca));}
-void start() {
+inline ll ask_way(int u, int v) {
+    ll res = 0;
+    for(;down[u] != down[v]; u = up[down[u]]) res += sumhld(0, 0, len[down[u]], d[down[u]] - d[u], len[down[u]], down[u]);
+    res += sumhld(0, 0, len[down[u]], d[down[u]] - d[u], d[down[v]] - d[v] + 1, down[u]);
+    return res;
+}
+inline void ch_way(int u, int v, int data) {
+    for(;down[u] != down[v];u = up[down[u]]) chhld(0, 0, len[down[u]], d[down[u]] - d[u], len[down[u]], down[u], data);
+    chhld(0, 0, len[down[u]], d[down[u]] - d[u], d[down[v]] - d[v] + 1, down[u], data);
+}
+//final functs
+void hld_build(){
     euler.reserve(2 * n - 1);
     dfs(0, -1);
     d[n] = n + 1;
-    l = 2 * n - 1;
-    buildlca(0, 0, l);
-    for (int i = 0; i < n; ++i)
+    buildlca(0, 0, euler.size());
+    for (int i = 0; i < n; ++i) {
         if (down[i] == i) {
-            t[i].resize(4 * len[i]);
+            t[i].resize(4 * len[i], 0);
+            c[i].resize(4 * len[i], 0);
+            is_ch[i].resize(4 * len[i], false);
             buildhld(0, 0, len[i], i);
         }
+    }
+}
+int hld_qwery(int u, int v){
+    int uvlca = lca(u, v);
+    return ask_way(u, uvlca) + ask_way(v, uvlca) - vdata[uvlca];
+}
+void hld_change(int u, int v, int  data){
+    int uvlca = lca(u, v);
+    ch_way(u, uvlca, data);
+    ch_way(v, uvlca, data);
+}
+//program
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    cin >> n;
+    for (int i = 0; i < n - 1; ++i) {
+        int u, v;
+        cin >> u >> v;
+        gr[u - 1].push_back(v - 1);
+        gr[v - 1].push_back(u - 1);
+    }
+    hld_build();
+    int q;
+    cin >> q;
+    for (int i = 0; i < q; ++i) {
+        int u, v, data;
+        char type;
+        cin >> type >> v >> u;
+        if (type == '?') {
+           cout << hld_qwery(u - 1, v - 1) << '\n';
+        } else {
+            cin >> data;
+            hld_change(u - 1, v - 1, data);
+        }
+    }
+    return 0;
 }
