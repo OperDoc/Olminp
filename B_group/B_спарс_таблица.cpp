03d9475756e0b46502cd58283f471e12c2c5f43d
Oper_doc <s.lit@outlook.com>
2019-03-04 22:15:45 +0300

    Update B_спарс_таблица.cpp

--- B_group/B_спарс_таблица.cpp
+++ B_group/B_спарс_таблица.cpp
@@ -1,14 +1,13 @@
-int sp[30][500010], arr[500010], m_log[500010], n;
-void st(){
-    for(int i = 0; i < 30; i++)
-        m_log[i] = log2(i);
-    for(int i = 0; i < n; i++)
-        sp[0][i] = arr[i];
-    for(int i = 1; (1<<i) < n; i++)
-        for(int j = 0; j + (1<<i) - 1 < n; j++)
-            sp[i][j] = min(sp[i - 1][j], sp[i - 1][j + (1<<(i - 1))]);
+int lg[500010], st[30][100010], a[500010], n;
+void start() {
+    for (int i = 2; i <= n; i++)
+        lg[i] = lg[i / 2] + 1;
+    for (int i = 0; (1 << i) <= n; i++) {
+        for (int j = 0; j + (1 << i) <= n; j++)
+            st[i][j] = i ? min(st[i - 1][j], st[i - 1][j + (1 << i - 1)]) : a[j];
+    }
 }
-int q(int l, int r)/*O(1)*/{
-    int i = m_log[r - l + 1];
-    return min(sp[i][l], sp[i][r - (1<<j) + 1]);
+int q(int l, int r) {
+    int p = lg[r - l + 1];
+    return min(st[p][l], st[p][r - (1 << p) + 1]);
 }
