470a3f52c7e9b2fe894fcbca3cf2b2045034fa9a
Oper_doc <s.lit@outlook.com>
2019-03-28 20:17:50 +0300

    Update A_максимальное_паросочитание.cpp

--- A_group/A_максимальное_паросочитание.cpp
+++ A_group/A_максимальное_паросочитание.cpp
@@ -1,17 +1,17 @@
 int V1, V2;
 vector< vector<int> > PLg;
 vector<int> Pairs;
-vector<bool> u1;
+vector<bool> u;
 bool try_k(int v)/*O(E)*/{
-    if(u1[v]) return false;
+    if(u[v]) return false;
     for(auto i:PLg[v])
-        if(mt[i] == -1 || try_k(mt[i])) return 1 + mt[i] = v;
+        if(mt[i] == -1 || (!u[mt[i]] && try_k(mt[i]))) return 1 + mt[i] = v;
     return false;
 }
 void kuhn()/*O(V * E)*/{
     mt.assign (V2, -1);
     for (int i = 0; i < V1; ++i) {
-        u1.assign(V1, false);
+        u.assign(V1, false);
         try_k(i);
     }
 }
